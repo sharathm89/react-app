313eb26f1ca9bc175fe5fafcf3a78f6ef8940a49
Sharath <sharath.nhce@gmail.com>
2020-12-27 10:12:13 +0530

    load fonts

--- src/app.js
+++ src/app.js
@@ -1,19 +1,26 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import { Provider } from "react-redux";
 
 import MainApp from "./container";
 
+import loadFonts from "Helpers/font";
 import { configureStore } from "./store";
 
-const App = () => (
-  <Provider store={configureStore()}>
-    <Router>
-      <Switch>
-        <Route path="/" component={MainApp} />
-      </Switch>
-    </Router>
-  </Provider>
-);
+const App = () => {
+  useEffect(() => {
+    loadFonts();
+  }, []);
+
+  return (
+    <Provider store={configureStore()}>
+      <Router>
+        <Switch>
+          <Route path="/" component={MainApp} />
+        </Switch>
+      </Router>
+    </Provider>
+  );
+};
 
 export default App;
