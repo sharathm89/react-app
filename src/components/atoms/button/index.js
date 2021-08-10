a73efc75997a4e7ebc5d244f7686f493a1f82dbf
Sharath <sharath.nhce@gmail.com>
2021-08-10 15:32:03 +0530

    add button

--- src/components/atoms/button/index.js
+++ src/components/atoms/button/index.js
@@ -3,19 +3,28 @@ import { func, string } from "prop-types";
 
 import "./button.css";
 
-const Button = ({ content, onClick }) => (
-  <button styleName="wrapper" onClick={onClick}>
+const Button = ({ content, onClick, type, className, ...props }) => (
+  <button
+    styleName={`wrapper ${type}`}
+    onClick={onClick}
+    className={className}
+    {...props}
+  >
     {content}
   </button>
 );
 
 Button.defaultProps = {
   content: "",
+  type: "type2",
+  className: "button",
 };
 
 Button.propTypes = {
   onClick: func,
   content: string,
+  type: string,
+  className: string,
 };
 
 export default Button;
--- src/components/atoms/button/stories.js
+++ src/components/atoms/button/stories.js
@@ -3,6 +3,10 @@ import React from "react";
 import { withStore } from "../../../../stories";
 import Button from "./index";
 
-withStore("Atoms/Button").add("default", () => (
-  <Button content="Login" onClick={() => {}} />
-));
+withStore("Atoms/Button")
+  .add("type1", () => (
+    <Button content="Log In" type="type1" onClick={() => {}} />
+  ))
+  .add("type2", () => (
+    <Button content="Submit" type="type2" onClick={() => {}} />
+  ));
